8dbaf37594a8517f4c6c3d2d4bcb26d013cd8d67
Samantha Felder <LE1937911@crc-lennox.qc.ca>
2020-11-18 23:52:51 -0500

    aight

--- FinalProject/Clients.Designer.cs
+++ FinalProject/Clients.Designer.cs
@@ -28,45 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.clientsListBox = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.clientListView = new System.Windows.Forms.ListView();
             this.SuspendLayout();
-            // 
-            // clientsListBox
-            // 
-            this.clientsListBox.FormattingEnabled = true;
-            this.clientsListBox.Location = new System.Drawing.Point(31, 57);
-            this.clientsListBox.Name = "clientsListBox";
-            this.clientsListBox.Size = new System.Drawing.Size(511, 251);
-            this.clientsListBox.TabIndex = 0;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(256, 19);
+            this.label4.Location = new System.Drawing.Point(236, 19);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(73, 24);
             this.label4.TabIndex = 12;
             this.label4.Text = "Clients";
             // 
+            // clientListView
+            // 
+            this.clientListView.HideSelection = false;
+            this.clientListView.Location = new System.Drawing.Point(21, 46);
+            this.clientListView.Name = "clientListView";
+            this.clientListView.Size = new System.Drawing.Size(531, 285);
+            this.clientListView.TabIndex = 13;
+            this.clientListView.UseCompatibleStateImageBehavior = false;
+            // 
             // Clients
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(581, 346);
+            this.ClientSize = new System.Drawing.Size(572, 346);
+            this.Controls.Add(this.clientListView);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.clientsListBox);
             this.Name = "Clients";
             this.Text = "Clients";
+            this.Load += new System.EventHandler(this.Clients_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox clientsListBox;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView clientListView;
     }
 }
--- FinalProject/Clients.cs
+++ FinalProject/Clients.cs
@@ -7,14 +7,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace FinalProject
 {
     public partial class Clients : Form
     {
+
+        //Constants to use when creating DB connections
+        private const string DbServerHost = "localhost";
+        private const string DbUsername = "postgres";
+        private const string DbUuserPassword = "peteypete117";
+        private const string DbName = "OOP";
+
+        NpgsqlConnection dbConnection;
+
         public Clients()
         {
             InitializeComponent();
+            SetDBConnection(DbServerHost, DbUsername, DbUuserPassword, DbName);
+            GetClientsFromDB();
+
+            clientListView.View = View.Details;
+            clientListView.FullRowSelect = true;
+            clientListView.GridLines = true;
+            clientListView.Columns.Add("ID");
+            clientListView.Columns.Add("Name");
+            clientListView.Columns.Add("Username");
+            clientListView.Columns.Add("Password");
+            clientListView.Columns.Add("Email");
+            clientListView.Columns.Add("SignUp Time");
+
+        }
+        List<Member> foundMembers = new List<Member>();
+
+        private void SetDBConnection(string serverAddress, string username, string passwd, string dbName)
+        {
+            string conectionString = "Host=" + serverAddress + "; Username=" + username + "; Password=" + passwd + "; Database=" + dbName + ";";
+
+            dbConnection = new NpgsqlConnection(conectionString);
+        }
+
+        private NpgsqlConnection CreateDBConnection(string serverAddress, string username, string passwd, string dbName)
+        {
+            string conectionString = "Host=" + serverAddress + "; Username=" + username + "; Password=" + passwd + "; Database=" + dbName + ";";
+
+            dbConnection = new NpgsqlConnection(conectionString);
+
+            return dbConnection;
+        }
+
+        private List<Member> GetClientsFromDB()
+        {
+            Member currentMember;
+
+            dbConnection.Open();
+
+            // This is a string representing the SQL query to execute in the db.
+            string sqlQuery = "SELECT * FROM user_account;";
+
+            NpgsqlCommand dbCommand = new NpgsqlCommand(sqlQuery, dbConnection);
+
+            NpgsqlDataReader dataReader = dbCommand.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                currentMember = new Member();
+
+                currentMember.Id = dataReader.GetInt32(0);
+                currentMember.Name = dataReader.GetString(1);
+                currentMember.Password = dataReader.GetString(2);
+                currentMember.Email = dataReader.GetString(3);
+                currentMember.Type = dataReader.GetInt32(4);
+                currentMember.SignUpTime = dataReader.GetDateTime(5);
+                currentMember.Username = dataReader.GetString(6);
+
+                foundMembers.Add(currentMember);
+
+                if (currentMember.Type == 1)
+                {
+                    clientListView.Items.Add(new ListViewItem(new[] { currentMember.Id.ToString(), currentMember.Name, currentMember.Username, currentMember.Password, currentMember.Email, currentMember.SignUpTime.ToString()}));
+                }
+            }
+
+            dbConnection.Close();
+
+            return foundMembers;
+        }
+
+        private void Clients_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
